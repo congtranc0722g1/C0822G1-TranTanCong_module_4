f54e9023ed7b197e7513f73f498c87e9484e4418
congtranc0722g1 <trantancongall@gmail.com>
2022-12-14 15:46:02 +0700

    nop

--- ss1_spring_overview/execise/dictionary/src/main/java/com/dictionary/service/impl/DictionaryService.java
+++ ss1_spring_overview/execise/dictionary/src/main/java/com/dictionary/service/impl/DictionaryService.java
@@ -5,29 +5,18 @@ import com.dictionary.service.IDictionaryService;
 public class DictionaryService implements IDictionaryService {
     @Override
     public String dictionary(String keyWord) {
-        String result;
-        switch (keyWord) {
-            case "Con mèo":
-                result = "Cat";
-                break;
-            case "Con chó":
-                result = "Dog";
-                break;
-            case "Con trâu":
-                result = "Buffalo";
-                break;
-            case "Con chim":
-                result = "Bird";
-                break;
-            case "Con lợn":
-                result = "Pigs";
-                break;
-            case "Con sư tử":
-                result = "The lion";
-                break;
-            default:
-                result = "Không tìm thấy";
+        String check = "Không tìm thấy";
+        String[] vietnamese = {"Con mèo", "Con chó", "Con trâu", "Con chim", "Con lợn", "Con sư tử"};
+        String[] english = {"Cat", "Dog", "Buffalo", "Bird", "Pigs", "The lion"};
+        for (int i = 0; i < vietnamese.length; i++) {
+            if (keyWord.equals(vietnamese[i])) {
+                for (int j = 0; j < english.length; j++) {
+                    if (i == j) {
+                        check = english[j];
+                    }
+                }
+            }
         }
-        return result;
+        return check;
     }
 }
--- ss1_spring_overview/execise/dictionary/src/main/webapp/WEB-INF/views/dictionary/translate.jsp
+++ ss1_spring_overview/execise/dictionary/src/main/webapp/WEB-INF/views/dictionary/translate.jsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>translate</title>
 </head>
 <body>
 <form action="/translate" method="get">
